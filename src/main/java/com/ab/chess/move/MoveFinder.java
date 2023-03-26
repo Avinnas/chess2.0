a6b2745fa2118835f44a5e397d89471110097ae0
Avinnas <baranadrian98@gmail.com>
2023-03-26 22:49:22 +0200

    move finder basic methods

--- src/main/java/com/ab/chess/move/MoveFinder.java
+++ src/main/java/com/ab/chess/move/MoveFinder.java
@@ -1,4 +1,15 @@
 package com.ab.chess.move;
 
+import com.ab.chess.position.Position;
+
+import java.util.List;
+
 public class MoveFinder {
+    public List<Move> findLegalMoves(Position position){
+        return findPseudoLegalMoves(position);
+    }
+
+    public List<Move> findPseudoLegalMoves(Position position){
+        return null;
+    }
 }
