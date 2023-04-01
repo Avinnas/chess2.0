43af5eeede2ad482d45911872d2d658f3a282cfe
Avinnas <baranadrian98@gmail.com>
2023-04-01 20:52:51 +0200

    Add tests for knight moves

--- src/test/java/com/ab/chess/piece/KnightTest.java
+++ src/test/java/com/ab/chess/piece/KnightTest.java
@@ -1,51 +1,52 @@
 package com.ab.chess.piece;
 
 import com.ab.chess.position.Position;
+import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Named;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class KnightTest {
 
-    @Test
-    void findPossibleTilesToMove1_LeftDownCorner() {
+    @ParameterizedTest(name = "{index}: {0} ")
+    @MethodSource("arguments")
+    void findPossibleTilesToMove_emptyBoard_returnsCorrectMoves(KnightTestData knightTestData) {
         Position position = new Position();
         Knight knight = new Knight(Color.WHITE);
-        position.putPiece(0, knight);
-        List<Integer> moves = knight.findPossibleTilesToMove(position, 0);
+        position.putPiece(knightTestData.input, knight);
+        List<Integer> moves = knight.findPossibleTilesToMove(position, knightTestData.input);
 
-        assertThat(moves).hasSameElementsAs(List.of(10, 17));
+        assertThat(moves).hasSameElementsAs(knightTestData.output);
     }
 
-    @Test
-    void findPossibleTilesToMove2_rightDownCorner() {
-        Position position = new Position();
-        Knight knight = new Knight(Color.WHITE);
-        position.putPiece(7, knight);
-        List<Integer> moves = knight.findPossibleTilesToMove(position, 7);
-
-        assertThat(moves).hasSameElementsAs(List.of(13, 22));
+    static Stream<Arguments> arguments() {
+        return Stream.of(
+                Arguments.of(Named.named("Knight in left down corner", new KnightTestData(0, List.of(10, 17)))),
+                Arguments.of(Named.named("Knight in right down corner", new KnightTestData(7, List.of(13, 22)))),
+                Arguments.of(Named.named("Knight in left up corner", new KnightTestData(56, List.of(41, 50)))),
+                Arguments.of(Named.named("Knight in right up corner", new KnightTestData(63, List.of(53, 46)))),
+                Arguments.of(Named.named("Knight at 0th row", new KnightTestData(3, List.of(9, 18, 20, 13)))),
+                Arguments.of(Named.named("Knight at 7th row", new KnightTestData(60, List.of(54, 45, 43, 50)))),
+                Arguments.of(Named.named("Knight at 0th column", new KnightTestData(24, List.of(9, 18, 34, 41)))),
+                Arguments.of(Named.named("Knight at 7th column", new KnightTestData(39, List.of(22, 29, 45, 54)))),
+                Arguments.of(Named.named("Knight at 1th row", new KnightTestData(12, List.of(2, 18, 27, 29, 22, 6)))),
+                Arguments.of(Named.named("Knight at 6th row", new KnightTestData(52, List.of(58, 42, 35, 37, 46, 62)))),
+                Arguments.of(Named.named("Knight at 1th column", new KnightTestData(25, List.of(8, 10, 19, 35, 40, 42)))),
+                Arguments.of(Named.named("Knight at 6th column", new KnightTestData(38, List.of(23, 21, 28, 44, 53, 55)))),
+                Arguments.of(Named.named("Knight in the center", new KnightTestData(28, List.of(11, 18, 13, 22, 38, 45, 43, 34))))
+        );
     }
 
-    @Test
-    void findPossibleTilesToMove_leftUpCorner() {
-        Position position = new Position();
-        Knight knight = new Knight(Color.WHITE);
-        position.putPiece(56, knight);
-        List<Integer> moves = knight.findPossibleTilesToMove(position, 56);
-
-        assertThat(moves).hasSameElementsAs(List.of(41, 50));
-    }
-
-    @Test
-    void findPossibleTilesToMove4_rightUpCorner() {
-        Position position = new Position();
-        Knight knight = new Knight(Color.WHITE);
-        position.putPiece(63, knight);
-        List<Integer> moves = knight.findPossibleTilesToMove(position, 63);
-
-        assertThat(moves).hasSameElementsAs(List.of(53, 46));
+    @AllArgsConstructor
+    public static class KnightTestData {
+        private int input;
+        private List<Integer> output;
     }
 }
