e962702be78a9f183419fe62d61cfba04670c238
Avinnas <baranadrian98@gmail.com>
2023-04-08 21:08:54 +0200

    remove unnecessary MoveList class

--- src/main/java/com/ab/chess/piece/Knight.java
+++ src/main/java/com/ab/chess/piece/Knight.java
@@ -1,23 +1,23 @@
 package com.ab.chess.piece;
 
 import com.ab.chess.move.Direction;
-import com.ab.chess.move.MoveList;
 import com.ab.chess.move.TileIterator;
 import com.ab.chess.position.Position;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Knight extends Piece {
 
   private static final List<Direction> moveDirections =
-          List.of(
-                  new Direction(2, 1),
-                  new Direction(2, -1),
-                  new Direction(-2, 1),
-                  new Direction(-2, -1),
-                  new Direction(-1, 2),
-                  new Direction(-1, -2),
-                  new Direction(1, -2),
-                  new Direction(1, 2));
+      List.of(
+          new Direction(2, 1),
+          new Direction(2, -1),
+          new Direction(-2, 1),
+          new Direction(-2, -1),
+          new Direction(-1, 2),
+          new Direction(-1, -2),
+          new Direction(1, -2),
+          new Direction(1, 2));
 
   public Knight(Color color) {
     super(color);
@@ -26,15 +26,24 @@ public class Knight extends Piece {
   @Override
   public List<Integer> findPossibleTilesToMove(Position position, int tileIndex) {
 
-    MoveList moveList = new MoveList(position,  this);
+    List<Integer> moves = new ArrayList<>();
 
-    for (Direction direction : moveDirections){
+    for (Direction direction : moveDirections) {
       TileIterator iterator = new TileIterator(tileIndex, direction);
-      if(iterator.hasNext()){
-        moveList.add(iterator.next());
+      if (iterator.hasNext()) {
+        int currentTile = iterator.next();
+        if (moveIsValid(position, tileIndex)) {
+          moves.add(currentTile);
+        }
       }
     }
+    return moves;
+  }
 
-    return moveList.getMoves();
+  private boolean moveIsValid(Position position, int tile) {
+    if (position.tileOccupiedByFriend(tile, color)) {
+      return false;
+    }
+    return true;
   }
 }
