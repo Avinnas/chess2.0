1c70a1f045d511383cf8b60f548ecf9cc5782ced
Avinnas <baranadrian98@gmail.com>
2023-04-09 00:34:52 +0200

    reformat

--- src/test/java/com/ab/chess/piece/RookTest.java
+++ src/test/java/com/ab/chess/piece/RookTest.java
@@ -1,28 +1,32 @@
 package com.ab.chess.piece;
 
-import com.ab.chess.position.Position;import org.junit.jupiter.api.Test;import java.util.List;import static org.assertj.core.api.Assertions.assertThat;public class RookTest {
-    @Test
-    void findPossibleMoves_defaultStartingBoard_returnsCorrectMoves() {
-        Rook rook = new Rook(Color.WHITE);
-        Position position = Position.createStartingPosition();
-        position.putPiece(16, rook);
+import com.ab.chess.position.Position;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 
-        List<Integer> actual = rook.findPossibleMoves(position, 16);
-        List<Integer> expected = List.of( 24, 32, 40, 48, 17, 18, 19, 20, 21, 22, 23);
+public class RookTest {
+  @Test
+  void findPossibleMoves_defaultStartingBoard_returnsCorrectMoves() {
+    Rook rook = new Rook(Color.WHITE);
+    Position position = Position.createStartingPosition();
+    position.putPiece(16, rook);
 
-        assertThat(actual).hasSameElementsAs(expected);
-    }
+    List<Integer> actual = rook.findPossibleMoves(position, 16);
+    List<Integer> expected = List.of(24, 32, 40, 48, 17, 18, 19, 20, 21, 22, 23);
 
-    @Test
-    void findControlledTiles_defaultStartingBoard_returnsCorrectMoves() {
-        Rook rook = new Rook(Color.WHITE);
-        Position position = Position.createStartingPosition();
-        position.putPiece(16, rook);
+    assertThat(actual).hasSameElementsAs(expected);
+  }
 
-        List<Integer> actual = rook.findControlledTiles(position, 16);
-        List<Integer> expected = List.of( 24, 32, 40, 48, 17, 18, 19, 20, 21, 22, 23, 8);
+  @Test
+  void findControlledTiles_defaultStartingBoard_returnsCorrectMoves() {
+    Rook rook = new Rook(Color.WHITE);
+    Position position = Position.createStartingPosition();
+    position.putPiece(16, rook);
 
-        assertThat(actual).hasSameElementsAs(expected);
-    }
+    List<Integer> actual = rook.findControlledTiles(position, 16);
+    List<Integer> expected = List.of(24, 32, 40, 48, 17, 18, 19, 20, 21, 22, 23, 8);
 
+    assertThat(actual).hasSameElementsAs(expected);
+  }
 }
