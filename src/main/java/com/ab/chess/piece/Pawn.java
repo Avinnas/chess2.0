3944ecb626e1b74e399c9d21dc0ab03076b2c4e6
Avinnas <baranadrian98@gmail.com>
2023-04-09 16:24:02 +0200

    Pawn - testing and debug

--- src/main/java/com/ab/chess/piece/Pawn.java
+++ src/main/java/com/ab/chess/piece/Pawn.java
@@ -1,12 +1,11 @@
 package com.ab.chess.piece;
 
 import com.ab.chess.move.Direction;
+import com.ab.chess.move.SearchMode;
 import com.ab.chess.move.TileIterator;
 import com.ab.chess.position.Position;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;import java.util.Set;
+import java.util.*;
 
 public class Pawn extends Piece {
   private static final Map<Color, Set<Integer>> STARTING_TILES =
@@ -19,37 +18,74 @@ public class Pawn extends Piece {
   private static final Map<Color, Direction> MOVE_DIRECTIONS =
       Map.of(Color.WHITE, Direction.UP, Color.BLACK, Direction.DOWN);
 
+  private static final Map<Color, Set<Direction>> CAPTURE_DIRECTIONS =
+      Map.of(
+          Color.WHITE, Set.of(Direction.LEFT_UP, Direction.RIGHT_UP),
+          Color.BLACK, Set.of(Direction.LEFT_DOWN, Direction.RIGHT_DOWN));
+
   public Pawn(Color color) {
     super(color);
   }
 
   @Override
   public List<Integer> findPossibleMoves(Position position, int pieceTileIndex) {
-    TileIterator iterator = new TileIterator(pieceTileIndex, getDirection());
-    List<Integer> moves = new ArrayList<>();
-
-    if (!iterator.hasNext()) {
-      return moves;
-    }
-    int currentTile = iterator.next();
-
-    if (position.tileEmpty(currentTile)) {
-      moves.add(currentTile);
-    }
-
-    if(!iterator.hasNext()){
-      return moves;
-    }
-    currentTile = iterator.next();
-    if(STARTING_TILES.get(color).contains(pieceTileIndex) && position.tileEmpty(currentTile)){
-      moves.add(currentTile);
-    }
-    return moves;
+    return findTiles(position, pieceTileIndex, SearchMode.POSSIBLE_MOVES);
   }
 
   @Override
   public List<Integer> findControlledTiles(Position position, int pieceTileIndex) {
-    return null;
+    return findTiles(position, pieceTileIndex, SearchMode.CONTROLLED_TILES);
+  }
+  // TODO: refactor to make it more readable
+
+  private List<Integer> findTiles(Position position, int pieceTileIndex, SearchMode searchMode) {
+    List<Integer> moves = new ArrayList<>();
+
+    for (Direction direction : CAPTURE_DIRECTIONS.get(color)) {
+      TileIterator captureIterator = new TileIterator(pieceTileIndex, direction);
+      if (captureIterator.hasNext()) {
+        int currentTile = captureIterator.next();
+        switch (searchMode) {
+          case POSSIBLE_MOVES -> {
+            if (position.tileOccupiedByEnemy(currentTile, color)) {
+              moves.add(currentTile);
+            }
+          }
+          case CONTROLLED_TILES -> {
+            moves.add(currentTile);
+          }
+        }
+      }
+    }
+    if(searchMode == SearchMode.POSSIBLE_MOVES){
+      List<Integer> nonCapturingMoves = findNonCapturingMoves(position, pieceTileIndex);
+      moves.addAll(nonCapturingMoves);
+    }
+    return moves;
+  }
+
+  private List<Integer> findNonCapturingMoves(Position position, int pieceTileIndex) {
+    List<Integer> moves = new ArrayList<>();
+    TileIterator iterator = new TileIterator(pieceTileIndex, getDirection());
+
+    if (!iterator.hasNext()) {
+      throw new IllegalStateException("Pawn reached last rank - it should be promoted before.");
+    }
+    int currentTile = iterator.next();
+
+    if (!position.tileEmpty(currentTile)) {
+      return moves;
+    }
+    moves.add(currentTile);
+
+    if (!iterator.hasNext()) {
+      return moves;
+    }
+    currentTile = iterator.next();
+    if (STARTING_TILES.get(color).contains(pieceTileIndex) && position.tileEmpty(currentTile)) {
+      moves.add(currentTile);
+    }
+    return moves;
   }
 
   private Direction getDirection() {
--- src/test/java/com/ab/chess/piece/PawnTest.java
+++ src/test/java/com/ab/chess/piece/PawnTest.java
@@ -1,21 +1,108 @@
 package com.ab.chess.piece;
 
-import com.ab.chess.position.Position;
-import org.junit.jupiter.api.Test;
-import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+import com.ab.chess.position.Position;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
 class PawnTest {
 
   @Test
-  void findPossibleMoves() {
+  void findPossibleMoves_pawnOnStartingPosition_returnsStartingMoves() {
     Pawn pawn = new Pawn(Color.WHITE);
-    Position position = Position.createStartingPosition();
+    Position position = new Position();
     List<Integer> actual = pawn.findPossibleMoves(position, 8);
 
     List<Integer> expected = List.of(16, 24);
+    assertThat(actual).hasSameElementsAs(expected);
+  }
+  @Test
+  void findPossibleMoves_pawnNotOnStartingPosition_returnsOneMove() {
+    Pawn pawn = new Pawn(Color.WHITE);
+    Position position = new Position();
+    List<Integer> actual = pawn.findPossibleMoves(position, 16);
 
+    List<Integer> expected = List.of(24);
+    assertThat(actual).hasSameElementsAs(expected);
+  }
+
+  @Test
+  void findPossibleMoves_pawnOnStartingPositionAndBlocked_returnsEmptyMoveList() {
+    Pawn pawn = new Pawn(Color.WHITE);
+    Position position = Position.createStartingPosition();
+    Pawn enemy = new Pawn(Color.BLACK);
+    position.putPiece(16, enemy);
+
+    List<Integer> actual = pawn.findPossibleMoves(position, 8);
+
+    List<Integer> expected = List.of();
+    assertThat(actual).hasSameElementsAs(expected);
+  }
+
+  @Test
+  void findPossibleMoves_pawnOnStartingPositionAndSecondTileBlocked_returnsEmptyMoveList() {
+    Pawn pawn = new Pawn(Color.WHITE);
+    Position position = Position.createStartingPosition();
+    Pawn enemy = new Pawn(Color.BLACK);
+    position.putPiece(24, enemy);
+
+    List<Integer> actual = pawn.findPossibleMoves(position, 8);
+
+    List<Integer> expected = List.of(16);
+    assertThat(actual).hasSameElementsAs(expected);
+  }
+
+  @Test
+  void findPossibleMoves_enemyOnControlledTile_canCaptureEnemy() {
+    Pawn pawn = new Pawn(Color.WHITE);
+    Position position = Position.createStartingPosition();
+    Pawn enemy = new Pawn(Color.BLACK);
+    position.putPiece(17, enemy);
+    
+    List<Integer> actual = pawn.findPossibleMoves(position, 8);
+
+    List<Integer> expected = List.of(16, 24, 17);
+    assertThat(actual).hasSameElementsAs(expected);
+  }
+
+  @Test
+  void findControlledTiles_emptyControlledTiles_returnsControlledTiles() {
+    Pawn pawn = new Pawn(Color.WHITE);
+    Position position = new Position();
+    List<Integer> actual = pawn.findControlledTiles(position, 9);
+
+    List<Integer> expected = List.of(16, 18);
+    assertThat(actual).hasSameElementsAs(expected);
+  }
+
+  @Test
+  void findControlledTiles_tileOccupiedByFriend_returnsControlledTiles() {
+    Pawn pawn = new Pawn(Color.WHITE);
+    Position position = new Position();
+    Pawn friend1 = new Pawn(Color.WHITE);
+    Pawn friend2 = new Pawn(Color.WHITE);
+    position.putPiece(16, friend1);
+    position.putPiece(18, friend2);
+    
+    List<Integer> actual = pawn.findControlledTiles(position, 9);
+
+    List<Integer> expected = List.of(16, 18);
+    assertThat(actual).hasSameElementsAs(expected);
+  }
+  @Test
+  void findControlledTiles_tileOccupiedByEnemy_returnsControlledTiles() {
+    Pawn pawn = new Pawn(Color.WHITE);
+    Position position = new Position();
+    Pawn enemy1 = new Pawn(Color.BLACK);
+    Pawn enemy2 = new Pawn(Color.BLACK);
+    position.putPiece(16, enemy1);
+    position.putPiece(18, enemy2);
+
+    List<Integer> actual = pawn.findControlledTiles(position, 9);
+
+    List<Integer> expected = List.of(16, 18);
     assertThat(actual).hasSameElementsAs(expected);
   }
 }
